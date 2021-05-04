a436f8acbc857c944a621b04968128cc7409f00d
yaegassy <yaegassy@users.noreply.github.com>
2021-05-04 20:35:22 +0900

    fix: windows support

--- src/installer.ts
+++ src/installer.ts
@@ -10,23 +10,28 @@ import { NGINX_LS_VERSION } from './constant';
 
 const exec = util.promisify(child_process.exec);
 
-export async function nginxLsInstall(context: ExtensionContext): Promise<void> {
+export async function nginxLsInstall(pythonCommand: string, context: ExtensionContext): Promise<void> {
   const pathVenv = path.join(context.storagePath, 'nginx-language-server', 'venv');
-  const pathPip = path.join(pathVenv, 'bin', 'pip');
+
+  let pathVenvPython = path.join(context.storagePath, 'pysen-ls', 'venv', 'bin', 'python');
+  if (process.platform === 'win32') {
+    pathVenvPython = path.join(context.storagePath, 'pysen-ls', 'venv', 'Scripts', 'python');
+  }
 
   const statusItem = window.createStatusBarItem(0, { progress: true });
-  statusItem.text = `Install nginx-language-server ...`;
+  statusItem.text = `Install nginx-language-server...`;
   statusItem.show();
 
   const installCmd =
-    `python3 -m venv ${pathVenv} && ` + `${pathPip} install -U pip nginx-language-server==${NGINX_LS_VERSION}`;
+    `${pathVenvPython} -m venv ${pathVenv} && ` +
+    `${pathVenvPython} -m pip install -U pip nginx-language-server==${NGINX_LS_VERSION}`;
 
   rimraf.sync(pathVenv);
   try {
-    window.showWarningMessage(`Install nginx-language-server...`);
+    window.showMessage(`Install nginx-language-server...`);
     await exec(installCmd);
     statusItem.hide();
-    window.showWarningMessage(`nginx-language-server: installed!`);
+    window.showMessage(`nginx-language-server: installed!`);
   } catch (error) {
     statusItem.hide();
     window.showErrorMessage(`nginx-language-server: install failed. | ${error}`);
